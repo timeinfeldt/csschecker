f3796e0f108376a2506e8bf91f645c4d9906dfda
Tim Einfeldt <tim.einfeldt@gmail.com>
2013-10-15 18:54:37 +0200

    added more info on RuleUsageCheck

--- src/checks/RuleUsageCheck.php
+++ src/checks/RuleUsageCheck.php
@@ -2,7 +2,27 @@
 namespace csschecker\checks;
 
 class RuleUsageCheck extends RuleCheck {
+
+    private $whiteList = array(
+        'color',
+        'line-height',
+        'font-size',
+        'padding',
+        'margin'
+    );
+
     public function run($rule) {
-        $this->addWarning($rule['name'], 'Defined ' . $rule['defCount'] . ' times with ' . count($rule['values']) . ' value(s).');
+        if (in_array($rule['name'], $this->whiteList)) {
+
+            usort($rule['values'], function($a, $b) {
+                return  $b['defCount'] - $a['defCount'];
+            });
+
+            $message = 'Defined ' . $rule['defCount'] . ' times with ' . count($rule['values']) . ' values: ' . PHP_EOL;
+            foreach($rule['values'] as $value) {
+                $message .= "\t" . $value['name'] . " (" . $value['defCount'] . ")" . PHP_EOL;
+            }
+            $this->addWarning($rule['name'], $message);
+        }
     }
 }
